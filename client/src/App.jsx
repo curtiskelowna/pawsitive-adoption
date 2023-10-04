701c254af304700df917c41eb8fbe0dd4a8a8cc5
edwin penuela <edwinalexander20@gmail.com>
2023-10-04 14:52:42 -0400

    update json

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
+import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/Signup';
-import './App.css';
 import HomePage from './routes/HomePage';
 
 function App() {

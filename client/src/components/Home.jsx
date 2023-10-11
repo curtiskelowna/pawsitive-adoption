551aba4cd669e70e6e24ca6ac1731d60a1050602
edwin penuela <edwinalexander20@gmail.com>
2023-10-10 20:28:04 -0400

    add style/paragraph to homepage

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -8,8 +8,11 @@ function Home({ addToFavorites, openModal, favorites, closeModal, selectedImage,
 
   return (
     <div className="home-page">
-      <div className="banner"/>
-      <p className="welcome">Welcome to our pet adoption shelter, where hearts are united with the paws that need them most. We're overjoyed to have you here, embarking on a journey filled with love, companionship, and wagging tails. At our shelter, we believe in the extraordinary power of rescue and the unbreakable bond between humans and their four-legged friends. Whether you're seeking the spirited energy of a playful pup, the comforting purrs of a feline friend, or the gentle wisdom of a senior companion, we're here to help you find your perfect furry match. Our shelter is more than a place for pets; it's a place for second chances, new beginnings, and countless heartwarming stories. Together, let's make the world a brighter and more compassionate place, one adoption at a time. Welcome to our family; welcome to a world of unconditional love.</p>
+      <section className="banner-container">
+      <div className="banner"></div>
+      </section>
+
+      <p className="welcome">Welcome to our pet adoption shelter, where hearts are united with the paws that need them most. We're overjoyed to have you here, embarking on a journey filled with love, companionship, and wagging tails.  Welcome to our family; welcome to a world of unconditional love.</p>
       <div className="search-container">
         <h1>New Furry Arrivals</h1>
         <SearchBar onSearch={searchPets} />

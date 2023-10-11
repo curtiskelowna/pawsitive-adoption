ee56a549ca3116f46837bfac6d0d24ac4861853a
edwin penuela <edwinalexander20@gmail.com>
2023-10-11 16:35:01 -0400

    update

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -11,7 +11,6 @@ function Home({ addToFavorites, openModal, favorites, closeModal, selectedImage,
       <section className="banner-container">
       <div className="banner"></div>
       </section>
-
       <p className="welcome">Welcome to our pet adoption shelter, where hearts are united with the paws that need them most. We're overjoyed to have you here, embarking on a journey filled with love, companionship, and wagging tails.  Welcome to our family; welcome to a world of unconditional love.</p>
       <div className="search-container">
         <h1>New Furry Arrivals</h1>
